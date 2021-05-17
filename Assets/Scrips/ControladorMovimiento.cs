0257450ff1936a2f02a327caa0615283fdb8e205
manolenta7 <manolenta7@gmail.com>
2021-05-17 18:29:08 -0400

    p1 se puede mover con las flechas para realizar pruebas

--- Assets/Scrips/ControladorMovimiento.cs
+++ Assets/Scrips/ControladorMovimiento.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ControladorMovimiento : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    Vector2 input;
+    public float speed; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = input * speed * Time.fixedDeltaTime;
     }
 }
